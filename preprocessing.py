47986676371a22f444be2abee1ce3b692175c6ab
Shubi <shubi@campus.technion.ac.il>
2020-11-18 01:59:39 +0200

    get data from db and add to each row info from the matching nc file

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,8 @@ from generate_atmosphere import RadiosondeProfile
 import re
 from netCDF4 import Dataset
 import sqlite3
+import fnmatch
+import matplotlib.pyplot as plt
 
 
 def gdas2radiosonde(src_file, dst_file, col_names=None):
@@ -177,9 +179,9 @@ def generate_daily_molecular_profile(gdas_dst_paths):
     return df_sigma, df_beta
 
 
-def extract_att_bsc(lidar_parent_folder, day_date, wavelengths):
+def extract_att_bsc(bsc_paths, wavelengths):
     """
-    For all .nc files under lidar_parent_folder with day_date dat, and for each wavelength in wavelengths
+    For all .nc files under bsc_paths and for each wavelength in wavelengths
     extract the OC_attenuated_backscatter_{wavelen}nm and Lidar_calibration_constant_used
 
     :param lidar_parent_folder: aath to netcdf folder
@@ -189,16 +191,16 @@ def extract_att_bsc(lidar_parent_folder, day_date, wavelengths):
     :return:
     """
 
-    bsc_paths, profile_paths = load_att_bsc(lidar_parent_folder, day_date)
     for wavelen in wavelengths:
         for bsc_path in bsc_paths:
             data = Dataset(bsc_path)
+            file_name = bsc_path.split('/')[-1]
             try:
                 vals = data.variables[f'OC_attenuated_backscatter_{wavelen}nm']
                 arr = vals * vals.Lidar_calibration_constant_used
-                print(f"Extracted OC_attenuated_backscatter_{wavelen}nm from {bsc_path.split('/')[-1]}")
+                print(f"Extracted OC_attenuated_backscatter_{wavelen}nm from {file_name}")
             except KeyError as e:
-                print(f"Key {e} does not exist in {bsc_path.split('/')[-1]}")
+                print(f"Key {e} does not exist in {file_name}")
 
 
 def query_database(query="SELECT * FROM lidar_calibration_constant;", database_path="pollyxt_tropos_calibration.db"):
@@ -224,24 +226,92 @@ def query_database(query="SELECT * FROM lidar_calibration_constant;", database_p
     with sqlite3.connect(database_path) as c:
         # Query to df
         # optionally parse 'id' as index column and 'cali_start_time', 'cali_stop_time' as dates
-        df = pd.read_sql(sql=query, con=c, index_col='id', parse_dates=['cali_start_time', 'cali_stop_time'])
+        df = pd.read_sql(sql=query, con=c, parse_dates=['cali_start_time', 'cali_stop_time'])
 
     return df
 
 
-
 def main():
+    DO_GDAS = True
+    DO_NETCDF = True
+
     """set day,location"""
-    lon = 35.0
-    lat = 32.8
     day_date = datetime(2017, 9, 1)
 
-    gdas_dst_paths = gdas_tropos2txt(day_date, lon, lat)
-    df_sigma, df_beta = generate_daily_molecular_profile(gdas_dst_paths)
+    # GDAS
+    if DO_GDAS:
+        lon = 35.0
+        lat = 32.8
+        gdas_dst_paths = gdas_tropos2txt(day_date, lon, lat)
+        df_sigma, df_beta = generate_daily_molecular_profile(gdas_dst_paths)
+        '''Visualize molecular profiles'''
+        plt.figure()
+        df_beta.plot()
+        plt.show()
 
-    wavelengths = ['355', '532', '1064']
-    lidar_parent_folder = 'data_example/data_examples/netcdf'
-    extract_att_bsc(lidar_parent_folder, day_date, wavelengths)
+
+    # NETCDF
+    if DO_NETCDF:
+
+        # Get the paths
+        lidar_parent_folder = 'data_example/data_examples/netcdf'
+        bsc_paths, profile_paths = load_att_bsc(lidar_parent_folder, day_date)
+
+        wavelengths = ['355', '532', '1064']
+
+        # Extract the OC_attenuated_backscatter_{wavelen}nm and Lidar_calibration_constant_used for all
+        # files in bsc_paths and for all wavelengths
+        # TODO do something with the data!
+        extract_att_bsc(bsc_paths, wavelengths)
+
+        # Query the db for a specfic day & aavelength and calib method
+        wavelength = 1064
+        day_diff = timedelta(days=1)
+        start_day = day_date.strftime('%Y-%m-%d')
+        till_date = (day_date + day_diff).strftime('%Y-%m-%d')
+        cali_method = 'Klett_Method'
+
+        query = f"""
+        SELECT lcc.liconst, lcc.cali_start_time, lcc.cali_stop_time, lcc.wavelength
+        FROM lidar_calibration_constant as lcc
+        WHERE
+            wavelength == {wavelength} AND
+            cali_method == '{cali_method}' AND
+            (cali_start_time BETWEEN '{start_day}' AND '{till_date}');
+        """
+
+        db_path = "data_example/pollyxt_tropos_calibration.db"
+        df = query_database(query=query, database_path=db_path)
+
+        # Build matching_nc_file
+        # TODO ?? currently matches any two values
+        df['nc_path'] = "*" + df['cali_start_time'].dt.strftime('%Y_%m_%d') + \
+                        "_" + df['cali_start_time'].dt.day_name().str.slice(start=0, stop=3) + \
+                        "_TROPOS_" + "??_00_01_" + \
+                        df['cali_start_time'].dt.strftime('%H%M') + "_" + \
+                        df['cali_stop_time'].dt.strftime('%H%M') + "_profiles.nc"
+
+        # Find Actual matching nc file (full path)
+        def get_file_match(x):
+            matched_file = fnmatch.filter(profile_paths, x)
+            if len(matched_file) == 1:
+                return matched_file[0]
+            else:
+                raise Exception  # make sure only one file is returned
+
+        df['matched_nc_file'] = df['nc_path'].apply(get_file_match)
+
+        # Get the altitude (r0) and delta_r
+        def get_info_from_profile_nc(s):
+            data = Dataset(s['matched_nc_file'])
+            wavelen = s.wavelength
+            delta_r = data.variables[f'reference_height_{wavelen}'][:].data[1] - \
+                      data.variables[f'reference_height_{wavelen}'][:].data[0]
+            return data.variables['altitude'][:].data.item(), delta_r
+
+        df[['altitude', 'delta_r']] = df.apply(get_info_from_profile_nc, axis=1, result_type='expand')
+        # TODO do something with the df
+        pass # add breakpoint here to see the df
 
 
 if __name__ == '__main__':
