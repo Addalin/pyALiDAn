edba601f6781b762b9552329f8eaaa97bec63ed0
Addalin <adi.vainiger@gmail.com>
2021-01-21 15:12:41 +0200

    version 0 of model based CNN adding tensorboard, and dumping models

--- data_loader.py
+++ data_loader.py
@@ -3,22 +3,17 @@ import warnings
 warnings.filterwarnings ( "ignore" )
 import pandas as pd
 import os
-from datetime import datetime , timedelta , time , date
-import glob
+from datetime import datetime , time
 import numpy as np
-import re
-from netCDF4 import Dataset
-import sqlite3
-import fnmatch
-import matplotlib.pyplot as plt
-import global_settings as gs
+
 import torch , torchvision
 import torch.utils.data
 import xarray as xr
-import matplotlib.dates as mdates
-from pytictoc import TicToc
-from multiprocessing import Pool , cpu_count
 import preprocessing as prep
+import torch.nn as nn
+import time
+from utils import create_and_configer_logger
+from torch.utils.tensorboard import SummaryWriter
 
 
 
@@ -26,7 +21,7 @@ class lidarDataSet ( torch.utils.data.Dataset ) :
     """TODO"""
 
     def __init__ ( self , csv_file , transform = None,
-                   top_height = 15.0 ) :
+                   top_height = 15.0 , Y_features = [ 'LC' , 'r0' , 'r1' ]) :
         """
         Args:
             csv_file (string): Path to the csv file of the database.
@@ -35,7 +30,7 @@ class lidarDataSet ( torch.utils.data.Dataset ) :
         self.data = pd.read_csv ( csv_file )
         self.key = [ 'date' , 'wavelength' , 'cali_method' , 'telescope' , 'cali_start_time' , 'cali_stop_time' ,
                      'start_time_period' , 'end_time_period' ]
-        self.Y_features = [ 'LC' , 'r0' , 'r1' ]  # , 'LC_std'
+        self.Y_features = Y_features # [ 'LC' , 'r0' , 'r1' ]  # , 'LC_std'
         self.X_features = [ 'lidar_path' , 'molecular_path' ]
         self.profiles = [ 'range_corr' , 'attbsc' ]
         self.top_height = top_height
@@ -97,7 +92,6 @@ class lidarDataSet ( torch.utils.data.Dataset ) :
         return Y
 
 
-
 class PowTransform(object):
     def __init__( self,powers = {'range_corr' : 0.5 , 'attbsc' : 0.5 ,
                              'LC' : 0.5 , 'LC_std' : 0.5 , 'r0' : 1 , 'r1' : 1} ):
@@ -150,42 +144,211 @@ class ToTensor ( object ) :
         X = X.permute ( 2 , 0 , 1 )
 
         # convert Y from pd.Series to np.array, and then to torch.tensor
-        Y = torch.from_numpy ( np.array ( Y ).astype ( float ) )
+        Y = torch.from_numpy ( np.array ( Y ).astype ( np.float32 ) )
 
         return {'x' : X ,'y' : Y}
 
+# TODO : apply poisson noise (miscLidar.generate_poisson_signal(mu, n)), add gaussian noise for augmentation
 
-def main( station_name = 'haifa' , start_date = datetime ( 2017 , 9 , 1 ) , end_date = datetime ( 2017 , 9 , 2 ) ):
+
+
+class DefaultCNN(nn.Module):
+
+    def __init__(self, in_channels=2, output_size=3, hidden_sizes=[16,32,64]):
+        super(DefaultCNN, self).__init__()
+
+        self.conv_layer = nn.Sequential(
+            # Conv layer 1
+            nn.Conv2d(in_channels=in_channels, out_channels=hidden_sizes[0], kernel_size=(5,3), padding=3),
+            nn.BatchNorm2d(hidden_sizes[0]),
+            nn.ReLU(inplace = True),
+            nn.Dropout2d ( p = 0.15 ),
+            nn.MaxPool2d ( kernel_size = (4,2) , stride = (4,2) ),
+
+            # Conv layer 2
+            nn.Conv2d(in_channels=hidden_sizes[0], out_channels=hidden_sizes[1], kernel_size=3, padding=1),
+            nn.BatchNorm2d(hidden_sizes[1]),
+            nn.ReLU(inplace = True),
+            nn.Dropout2d ( p = 0.15 ) ,
+            nn.MaxPool2d(kernel_size = (4,2), stride =(4,2)),
+
+            # Conv layer 2
+            nn.Conv2d ( in_channels = hidden_sizes [ 1 ] , out_channels = hidden_sizes [ 2 ] , kernel_size = 3 ,
+                        padding = 1 ) ,
+            nn.BatchNorm2d ( hidden_sizes [ 2 ] ) ,
+            nn.ReLU ( inplace = True ) ,
+            nn.Dropout2d ( p = 0.15 ) ,
+            nn.MaxPool2d ( kernel_size = (4 , 2) , stride = (4 , 2) ) ,
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Linear ( 8*32*8 , 512 ),
+            nn.ReLU ( inplace = True ) ,
+            nn.Dropout ( p = 0.1 ) ,
+            nn.Linear ( 512 , output_size ) ,
+        )
+
+    def forward(self, x):
+        #conv layers
+        x = self.conv_layer(x)
+
+        # flatten
+        x = x.view(x.shape[0], -1)
+
+        # fc layer # TODO: add option to change in_channels to fc_layer (when changing hidden sizes of CNN)
+        out = self.fc_layer(x)
+
+        return out
+def calculate_accuracy (model, loader,device):
+    """
+
+    :param model:
+    :param loader:
+    :param device:
+    :return:
+    """
+    model.eval ( )             # Evaluation mode
+    criterion = nn.MSELoss ( )
+    running_loss = 0.0
+    #with torch.no_grad: #TODO: why this is not working?
+    for i,sample in enumerate(loader):
+        x = sample [ 'x' ].to ( device )
+        y = sample [ 'y' ].to ( device )
+        y_pred = model(x)
+        loss = criterion(y,y_pred)
+        running_loss += loss.data.item ( )
+    running_loss /= len ( loader )
+    return  running_loss
+
+
+def main( station_name = 'haifa' , start_date = datetime ( 2017 , 9 , 1 ) , end_date = datetime ( 2017 , 9 , 2 ),
+          run_params={'model_n':0, 's_model':0,'powers':None,'lr':1e-3,'batch_size':4, 'Y_features':[ 'LC' , 'r0' , 'r1' ]} ):
+    #logger = create_and_configer_logger ( 'data_loader.log' ) #TODO: Why this is causing the program to fall?
+    # device - cpu or gpu?
+    device = torch.device ( "cuda:0" if torch.cuda.is_available ( ) else "cpu" )
+    print ( f"Using device: {device}")
+
 
     # Step 1. Load Dataset
-    csv_path = f"dataset_{station_name}_{start_date.strftime ( '%Y-%m-%d' )}_{end_date.strftime ( '%Y-%m-%d' )}.csv"
-    powers = {'range_corr' : 0.5 , 'attbsc' : 0.5 , 'LC' : 0.5 , 'LC_std' : 0.5 , 'r0' : 1 , 'r1' : 1}
-    lidar_transforms = torchvision.transforms.Compose([PowTransform(powers),ToTensor()])
-    dataset = lidarDataSet ( csv_path , lidar_transforms )
+    csv_path = f"dataset_{station_name}_{start_date.strftime ( '%Y-%m-%d' )}_{end_date.strftime ( '%Y-%m-%d' )}_on_D.csv"
+    powers = run_params['powers']
+    # TODO: add option - y = {bin(r0),bin(r1)}
+    lidar_transforms = torchvision.transforms.Compose([PowTransform(powers),ToTensor()]) if powers\
+                  else torchvision.transforms.Compose([ToTensor()])
+    dataset = lidarDataSet ( csv_path , lidar_transforms, top_height = 15.3, Y_features = run_params['Y_features'] )
     train_set , test_set = dataset.get_splits(n_test = 0.2)
 
+    # Step 2. Create Model Class
+
+    # Hyper parameters
+    model_n = run_params['model']
+    s_model = run_params['s_model']
+    in_channels = 2
+    output_size = len(run_params['Y_features'])
+    hidden_sizes = [ 16, 32, 8] # TODO: add option - hidden_sizes = [ 8, 16, 32], [16, 32, 8], [ 64, 32, 16]
+    batch_size = run_params['batch_size']
+    n_iters = 3000
+    epochs = int(round(n_iters / (len ( train_set ) / batch_size)))
+    learning_rate = run_params['lr']
+    print (f"Start train model. Hidden size:{hidden_sizes}, batch_size:{batch_size}, n_iters:{n_iters}, epochs:{epochs}")
+    use_pow = 'use_' if powers else 'no_'
+    run_name = f"{datetime.now().strftime('%Y_%m_%d_%H_%M')}_v{model_n}.{s_model}.{use_pow}pow_epochs_{epochs}_batch_size_{batch_size}_lr_{learning_rate}"
 
 
-    # Step 2. Split and Make Datasets Iterable
+    model = DefaultCNN(in_channels = in_channels,output_size = output_size,hidden_sizes = hidden_sizes).to(device)
+    numParams = 0
+    for parameter in model.parameters ( ) :
+        if (parameter.requires_grad) :
+            numParams += parameter.numel ( )
+    print( f"Number of parameters in model: {numParams}" )
 
 
+    # Step 3. Instantiate Loss Class
+    criterion = nn.MSELoss()
+
+    # Step 4. Instantiate Optimizer Class
+    optimizer = torch.optim.Adam ( model.parameters ( ) , lr = learning_rate )
+
+    # Step 5. Initiate dataloader
     train_loader = torch.utils.data.DataLoader (
-        train_set , batch_size = 4 , shuffle = True , num_workers = 7 )
+        train_set , batch_size = batch_size , shuffle = True , num_workers = 7 )
 
     test_loader = torch.utils.data.DataLoader (
-        test_set , batch_size = 4 , shuffle = False , num_workers = 7 )
+        test_set , batch_size = batch_size , shuffle = False , num_workers = 7 )
 
+    writer = SummaryWriter ( os.path.join('runs',run_name ))
+    # Training loop
+    for epoch in range(1, epochs+1):
+        model.train()   # Training mode
+        running_loss = 0.0
+        epoch_time = time.time ( )
+        for i_batch , sample_batched in enumerate ( train_loader ) :
+            # get inputs of x,y & send to device
+            x = sample_batched [ 'x' ].to ( device )
+            y = sample_batched [ 'y' ].to ( device )
 
-    for i_batch , sample_batched in enumerate ( train_loader ) :
-        print ( i_batch , sample_batched )
+            # forward + backward + optimize
+            y_pred = model(x)           # Forward
+            loss = criterion(y,y_pred)  # set loss calculation
+            optimizer.zero_grad()   # zeroing parameters gradient
+            loss.backward()         # backpropagation
+            optimizer.step()        # update parameters
 
-    for i_batch , sample_batched in enumerate ( test_loader ) :
-        print ( i_batch , sample_batched )
+            # for statistics
+            global_iter = len(train_loader)*(epoch-1)+i_batch
+            writer.add_scalar ( "batch_loss" , loss ,global_iter )
+            running_loss += loss.data.item()
 
+        # Normalizing loss by the total batches in train loader
+        running_loss/=len(train_loader)
+        writer.add_scalar ( "running_loss" , running_loss , epoch )
+
+        # Calculate training and test set MSEloss for current model
+        train_loss = calculate_accuracy(model, train_loader,device)
+        writer.add_scalar ( "train_loss" , train_loss , epoch )
+        test_loss = calculate_accuracy(model,test_loader,device)
+        writer.add_scalar ( "test_loss" , test_loss , epoch )
+        epoch_time = time.time ( ) - epoch_time
+        log = f"Epoch: {epoch} | Running MSE Loss: {running_loss:.4f} |" \
+              f" Training MSE loss: {train_loss:.3f} | Test MSE loss: {test_loss:.3f} | " \
+              f" Epoch Time: {epoch_time:.2f} secs"
+        # print()
+        print( log )
+
+        # Save the model
+
+        state = {
+            'model_state_dict' : model.state_dict ( ) ,
+            'optimizer_state_dict' : optimizer.state_dict() ,
+            'epoch': epoch,
+            'batch_size':batch_size,
+            'global_iter':global_iter,
+            'n_iters':n_iters,
+            'learning_rate': learning_rate
+        }
+        modelv_dir = f"checkpoints/model_{model_n}"
+        if not os.path.isdir ( modelv_dir ) :
+            os.mkdir (modelv_dir )
+        model_fname = f"{datetime.now ( ).strftime ( '%Y_%m_%d_%H_%M' )}_" \
+                      f"v{model_n}.{s_model}.{use_pow}pow_epoch_{epoch}-{epochs}_batch_size_{batch_size}_lr_{learning_rate}.pth"
+        torch.save ( state , os.path.join(modelv_dir,model_fname) )
+        print ( f"Finished training epoch {epoch}, saved model to: {model_fname}" )
+    writer.flush ( )
 
 
 if __name__ == '__main__' :
     station_name = 'haifa'
     start_date = datetime ( 2017 , 9 , 1 )
     end_date = datetime ( 2017 , 10 , 31 )
-    main ( station_name , start_date , end_date )
+    learning_rates = [1e-3, 1e-4]
+    batch_sizes = [8]
+    Y_features = [['r0' , 'r1'],['r0' , 'r1' , 'LC']]
+    powers = [None,{'range_corr' : 0.5 , 'attbsc' : 0.5 , 'LC' : 0.5 , 'LC_std' : 0.5 , 'r0' : 1 , 'r1' : 1}]
+    model_n = 0
+    for s_model,yf in enumerate(Y_features):
+        for lr in learning_rates:
+            for pow in powers :
+                for batch_size in batch_sizes:
+                    run_params = {'model':model_n, 's_model':s_model ,'powers':pow,
+                                  'Y_features': yf, 'lr':lr, 'batch_size':batch_size}
+                    main ( station_name , start_date , end_date, run_params )
