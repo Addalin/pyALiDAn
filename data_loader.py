208bd758e6e775e9c7403dcf8829e2fb2b76b3e8
Addalin <adi.vainiger@gmail.com>
2021-01-25 13:17:14 +0200

    creating MARELoss(_Loss) according to nn.L1Loss(), creating  mare_loss according to pytorch l1_loss() using mare loss for feature evaluation and adding feature loss to tensorboard updating write_hparams()

--- data_loader.py
+++ data_loader.py
@@ -14,6 +14,12 @@ import torch.nn as nn
 import time
 from utils import create_and_configer_logger
 from torch.utils.tensorboard import SummaryWriter
+from torch import functional as F
+from torch import Tensor
+from torch.nn.modules.loss import _Loss,_Reduction
+from torch.overrides import has_torch_function , handle_torch_function
+
+#from ignite.contrib.metrics.regression import MeanAbsoluteRelativeError #This method is not working yet
 torch.manual_seed(8318)
 import json
 
@@ -262,12 +268,12 @@ def calculate_statistics(model,criterion, run_params, loader,device, Y_features=
             if Y_features:
                 wavelength = sample['wavelength']
                 for idx_f , feature in enumerate ( Y_features ) :
-                    feature_loss [ feature ][ 'all' ] += MARELoss ( y_pred[ : , idx_f ] , y[ : , idx_f ] )
+                    feature_loss [ feature ][ 'all' ] += mare_loss ( y_pred[ : , idx_f ] , y[ : , idx_f ] ).data.item()
                     for wav in wavelengths :
                         idx_w = torch.where ( wavelength == wav )[0]
                         if idx_w.numel()>0 :
                            feature_loss [ feature ] [ wav ] += \
-                                MARELoss (  y_pred [ idx_w ][ : , idx_f ] , y[idx_w][ : , idx_f ])
+                                mare_loss (  y_pred [ idx_w ][ : , idx_f ] , y[idx_w][ : , idx_f ]).data.item()
                         else:
                             count_less[wav]+=1
     running_loss /= len ( loader )
@@ -281,15 +287,102 @@ def calculate_statistics(model,criterion, run_params, loader,device, Y_features=
     return  stats
 
 
-def MARELoss(y_pred, y):
+
+def mare_loss ( input , target , size_average = None , reduce = None , reduction = 'mean' ) :
+    # type: (Tensor, Tensor, Optional[bool], Optional[bool], str) -> Tensor
+
+    r""" Overriding l1_loss(input, target, size_average=None, reduce=None, reduction='mean') -> Tensor
+
+    Function that takes the mean element-wise absolute relative value difference.
+
+    See :class:`~torch.nn.L1Loss` for details.
     """
-    Calculates the Mean Absolute Relative Error
-    :param y_pred:
-    :param y:
-    :return: MAREloss between y and y_pred
+    if not torch.jit.is_scripting ( ) :
+        tens_ops = (input , target)
+        if any ( [ type ( t ) is not Tensor for t in tens_ops ] ) and has_torch_function ( tens_ops ) :
+            return handle_torch_function (
+                mare_loss , tens_ops , input , target , size_average = size_average , reduce = reduce ,
+                reduction = reduction )
+    if not (target.size ( ) == input.size ( )) :
+        warnings.warn ( "Using a target size ({}) that is different to the input size ({}). "
+                        "This will likely lead to incorrect results due to broadcasting. "
+                        "Please ensure they have the same size.".format ( target.size ( ) , input.size ( ) ) ,
+                        stacklevel = 2 )
+    if size_average is not None or reduce is not None :
+        reduction = _Reduction.legacy_get_string ( size_average , reduce )
+
+    expanded_input , expanded_target = torch.broadcast_tensors ( input , target )
+    loss = torch.mean ( torch.abs ((expanded_target - expanded_input)/expanded_target) )
+    #return torch._C._nn.l1_loss ( expanded_input/expanded_target , torch.div(expanded_target/expanded_target) , _Reduction.get_enum ( reduction ) )
+    return  loss
+
+class MARELoss(_Loss):
+    r"""Creates a criterion that measures the mean absolute relative error (MARE) between each element in
+    the input :math:`x` and target :math:`y`.
+
+    The unreduced (i.e. with :attr:`reduction` set to ``'none'``) loss can be described as:
+
+    .. math::
+        \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad
+        l_n = \frac{\left| x_n - y_n\right|}{\left| y_n \right|},
+
+    where :math:`N` is the batch size. If :attr:`reduction` is not ``'none'``
+    (default ``'mean'``), then:
+
+    .. math::
+        \ell(x, y) =
+        \begin{cases}
+            \operatorname{mean}(L), & \text{if reduction} = \text{'mean';}\\
+            \operatorname{sum}(L),  & \text{if reduction} = \text{'sum'.}
+        \end{cases}
+
+    :math:`x` and :math:`y` are tensors of arbitrary shapes with a total
+    of :math:`n` elements each.
+
+    The sum operation still operates over all the elements, and divides by :math:`n`.
+
+    The division by :math:`n` can be avoided if one sets ``reduction = 'sum'``.
+
+    Args:
+        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
+            the losses are averaged over each loss element in the batch. Note that for
+            some losses, there are multiple elements per sample. If the field :attr:`size_average`
+            is set to ``False``, the losses are instead summed for each minibatch. Ignored
+            when reduce is ``False``. Default: ``True``
+        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
+            losses are averaged or summed over observations for each minibatch depending
+            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
+            batch element instead and ignores :attr:`size_average`. Default: ``True``
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+            ``'mean'``: the sum of the output will be divided by the number of
+            elements in the output, ``'sum'``: the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
+            specifying either of those two args will override :attr:`reduction`. Default: ``'mean'``
+
+    Shape:
+        - Input: :math:`(N, *)` where :math:`*` means, any number of additional
+          dimensions
+        - Target: :math:`(N, *)`, same shape as the input
+        - Output: scalar. If :attr:`reduction` is ``'none'``, then
+          :math:`(N, *)`, same shape as the input
+
+    Examples::
+
+        >>> loss = nn.MARELoss()
+        >>> input = torch.randn(3, 5, requires_grad=True)
+        >>> target = torch.randn(3, 5)
+        >>> output = loss(input, target)
+        >>> output.backward()
     """
-    loss = torch.mean(torch.abs(y - y_pred)/y)
-    return loss
+    __constants__ = ['reduction']
+
+    def __init__(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+        super(MARELoss, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        return mare_loss(input, target, reduction=self.reduction)
+
 
 def update_stats(writer, model,loaders ,device,run_params,criterion, epoch):
     """
@@ -304,6 +397,7 @@ def update_stats(writer, model,loaders ,device,run_params,criterion, epoch):
     :return: curr_loss - current loss for train and for test
     """
     curr_loss = {}
+    feature_loss={}
     for loader, mode in zip(loaders,['train','test']):
         # calc current epoch statistics: train and test model, and feature loss for debug.
         stats = calculate_statistics ( model , criterion, run_params,loader , device,
@@ -317,6 +411,7 @@ def update_stats(writer, model,loaders ,device,run_params,criterion, epoch):
         curr_loss.update( {mode: field_value} )
 
         # add feature losses for debug
+        feature_loss.update({mode:{}})
         for feature in run_params [ 'Y_features' ] :
             for wav in run_params [ 'wavelengths' ] :
                 # add FeatureLoss (per wavelength per feature) , currently the metric is MAERLoss.
@@ -330,22 +425,32 @@ def update_stats(writer, model,loaders ,device,run_params,criterion, epoch):
             field_name = f"FeatureLoss_{mode}/{feature}"
             field_value = stats ['FeatureLoss'] [ feature] [ 'all' ]
             writer.add_scalar ( field_name , field_value , epoch )
-    return curr_loss
+            feature_loss[mode].update({feature:field_value})
+    return curr_loss,feature_loss
 
 
-def write_hparams(writer,run_params,run_name, cur_result, best_result):
+def write_hparams(writer,run_params,run_name, cur_loss, best_loss, cur_loss_feature,best_loss_feature):
+
+    results = {'hparam_last/loss_train' : cur_loss [ 'loss' ] [ 'train' ] ,
+               'hparam_last/loss_test' : cur_loss [ 'loss' ] [ 'test' ],
+               'hparam_last/epoch' : cur_loss [ 'epoch' ] ,
+               'hparam_best/loss_train' : best_loss [ 'loss' ] [ 'train' ] ,
+               'hparam_best/epoch_train' : best_loss [ 'epoch' ] [ 'train' ],
+               'hparam_best/loss_test' : best_loss [ 'loss' ] [ 'test' ] ,
+               'hparam_best/epoch_test' : best_loss [ 'epoch' ] [ 'test' ] ,
+               }
+    for mode in ['train','test']:
+        for feature in run_params['Y_features']:
+            results.update({f"hparam_{feature}_last/loss_{mode}" : cur_loss_feature [ 'loss' ][mode][ feature ]} )
+            results.update({f"hparam_{feature}_last/epoch_{mode}" : cur_loss_feature [ 'epoch' ]} )
+            results.update({f"hparam_{feature}_best/loss_{mode}":best_loss_feature['loss'][ mode ][feature]})
+            results.update({f"hparam_{feature}_best/epoch_{mode}":best_loss_feature['epoch'][ mode ][feature]})
 
     run_params [ 'Y_features' ] = '_'.join ( run_params [ 'Y_features' ] )
-    run_params [ 'hidden_sizes' ] = torch.from_numpy ( np.asarray ( run_params [ 'hidden_sizes' ] ) )
-    run_params [ 'wavelengths' ] = torch.from_numpy ( np.asarray ( run_params [ 'wavelengths' ] ) )
-    results = {'hparam_last/loss_train' : cur_result [ 'loss' ] [ 'train' ] ,
-               'hparam_last/loss_test' : cur_result [ 'loss' ] [ 'test' ],
-               'hparam_last/epoch' : cur_result [ 'epoch' ] ,
-               'hparam_best/loss_train' : best_result [ 'loss' ] [ 'train' ] ,
-               'hparam_best/epoch_train' : best_result [ 'epoch' ] [ 'train' ],
-               'hparam_best/loss_test' : best_result [ 'loss' ] [ 'test' ] ,
-               'hparam_best/epoch_test' : best_result [ 'epoch' ] [ 'test' ] ,
-               }
+    run_params [ 'hidden_sizes' ] = '_'.join ( [ str(val) for val in run_params [ 'hidden_sizes' ]] )
+    run_params [ 'wavelengths' ] = '_'.join ( [ str(val) for val in run_params [ 'wavelengths' ]] )
+    run_params ['powers'] = run_params['powers']['LC'] if run_params['powers'] is not None else None
+
     writer.add_hparams(hparam_dict = run_params,metric_dict =results,run_name=run_name )
 
 def main( station_name = 'haifa' , start_date = datetime ( 2017 , 9 , 1 ) , end_date = datetime ( 2017 , 9 , 2 ),
@@ -456,17 +561,29 @@ def main( station_name = 'haifa' , start_date = datetime ( 2017 , 9 , 1 ) , end_
         writer.add_scalar ( f"{loss_type}/running_loss" , running_loss , epoch )
 
         # Calculate statistics for current model
-        epoch_loss = update_stats ( writer , model , [train_loader,test_loader] , device , run_params , criterion, epoch )
+        epoch_loss, feature_loss = update_stats ( writer , model , [train_loader,test_loader] , device , run_params , criterion, epoch )
 
         # Save best loss (and the epoch number) for train and test
         if best_loss is None:
             best_loss = epoch_loss
             best_epoch = {'train': epoch, 'test':epoch}
+            best_feture_loss = feature_loss
+            best_feature_epoch={}
+            for mode in ['train','test']:
+                best_feature_epoch.update({mode:{}})
+                for feature in run_params['Y_features']:
+                    best_feature_epoch[mode].update({feature: epoch})
         else:
             for mode in ['train','test']:
                 if best_loss[mode]> epoch_loss[mode]:
                     best_loss [ mode ] = epoch_loss[mode]
                     best_epoch[ mode ] = epoch
+                for feature in run_params['Y_features']:
+                    if best_feture_loss [ mode ][feature] > feature_loss [ mode ][feature]:
+                        best_feture_loss [ mode ] [ feature ] = feature_loss [ mode ][feature]
+                        best_feature_epoch [ mode ][feature] = epoch
+
+
 
         epoch_time = time.time ( ) - epoch_time
 
@@ -495,7 +612,11 @@ def main( station_name = 'haifa' , start_date = datetime ( 2017 , 9 , 1 ) , end_
 
 
         print ( f"Finished training epoch {epoch}, saved model to: {model_fname}" )
-    write_hparams(writer,run_params, run_name='hparams', cur_result ={'loss':epoch_loss, 'epoch':epoch}, best_result={'loss':best_loss,'epoch':best_epoch})
+    write_hparams(writer,run_params, run_name='hparams',
+                  cur_loss ={'loss':epoch_loss, 'epoch':epoch},
+                  best_loss={'loss':best_loss,'epoch':best_epoch},
+                  cur_loss_feature={'loss':feature_loss,'epoch':epoch},
+                  best_loss_feature={'loss':best_feture_loss,'epoch':best_feature_epoch})
     writer.flush ( )
 
 
