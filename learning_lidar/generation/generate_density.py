5e2a02674da261c3133cbf79c6ff2b32f3718092
Addalin <adi.vainiger@gmail.com>
2021-04-24 15:41:50 +0300

    refactoring generate_density_utils.py imports

--- learning_lidar/generation/generate_density.py
+++ learning_lidar/generation/generate_density.py
@@ -5,6 +5,7 @@ import numpy as np
 import seaborn as sns
 import learning_lidar.global_settings as gs
 import learning_lidar.generation.generation_utils as gen_utils
+from learning_lidar.generation.generate_density_utils import generate_density, generate_aerosol
 
 from learning_lidar.utils.utils import create_and_configer_logger
 
