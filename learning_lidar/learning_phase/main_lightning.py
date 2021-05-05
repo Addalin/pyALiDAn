16d73a0a3716f330699815972eb64db9a7479a23
Shubi <shubi@campus.technion.ac.il>
2021-05-05 14:13:46 +0300

    seperate main_lightning.py to run_params.py and main_lightning.py

--- learning_lidar/learning_phase/main_lightning.py
+++ learning_lidar/learning_phase/main_lightning.py
@@ -3,10 +3,10 @@ import os.path
 from datetime import datetime
 
 import ray
-import torch.cuda
 from ray import tune
 from ray.tune import CLIReporter
 from ray.tune.integration.pytorch_lightning import TuneReportCallback, TuneReportCheckpointCallback
+from run_params import CONSTS, RAY_HYPER_PARAMS, RESULTS_PATH, NUM_AVAILABLE_GPU, NON_RAY_HYPER_PARAMS
 
 from pytorch_lightning import Trainer, seed_everything
 
@@ -52,7 +52,7 @@ def main(config, checkpoint_dir=None, consts=None):
     trainer = Trainer(max_epochs=consts['max_epochs'],
                       callbacks=callbacks,
                       gpus=[1] if consts['num_gpus'] > 0 else 0)
-    # trainer = Trainer(max_steps=consts['max_steps'])
+
     lidar_dm.setup('fit')
     trainer.fit(model=model, datamodule=lidar_dm)
 
@@ -62,70 +62,32 @@ def main(config, checkpoint_dir=None, consts=None):
 
 
 if __name__ == '__main__':
-    # Debug flag to enable debugging
+    USE_RAY = True
+    DEBUG_RAY = False
+
+    # Override number of workers if debugging
+    CONSTS['num_workers'] = 0 if DEBUG_RAY else CONSTS['num_workers']
+
     logger = create_and_configer_logger(
         log_name=f"{os.path.dirname(__file__)}_{datetime.now().strftime('%Y-%m-%d %H_%M_%S')}.log", level=logging.INFO)
-    max_workers = 6
-    num_available_gpu = torch.cuda.device_count()
-    DEBUG_RAY = True
+
     if DEBUG_RAY:
         ray.init(local_mode=True)
-    base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    data_params = {
-        'base_path': os.path.join(base_path, 'data'),
-        'station_name': 'haifa',
-        'start_date': datetime(2017, 9, 1),
-        'end_date': datetime(2017, 10, 31),
-    }
 
-    csv_base_name = f"dataset_gen_{data_params['station_name']}_" \
-                    f"{data_params['start_date'].strftime('%Y-%m-%d')}_" \
-                    f"{data_params['end_date'].strftime('%Y-%m-%d')}"
-    train_csv_path = os.path.join(data_params['base_path'], f'{csv_base_name}_train.csv')
-    test_csv_path = os.path.join(data_params['base_path'], f'{csv_base_name}_test.csv')
-
-    # Constants - should correspond to data, dataloader and model
-    consts = {
-        "fc_size": [512],
-        'max_epochs': 2,
-        'num_workers': 0 if DEBUG_RAY else max_workers,
-        'train_csv_path': train_csv_path,
-        'test_csv_path': test_csv_path,
-        'powers': {'range_corr': 0.5, 'attbsc': 0.5, 'p_bg': 0.5,
-                   'LC': 0.5, 'LC_std': 0.5, 'r0': 1, 'r1': 1, 'dr': 1},
-        'num_gpus': num_available_gpu,
-    }
-
-    # Defining a search space
-    # Note, replace choice with grid_search if want all possible combinations
-    use_ray = True
-    if use_ray:
-        hyper_params = {
-            "hidden_sizes": tune.choice([[16, 32, 8]]), # TODO: add options of [ 8, 16, 32], [16, 32, 8], [ 64, 32, 16]
-            "lr": tune.grid_search([1e-3, 0.5 * 1e-3, 1e-4]),
-            "bsize": tune.choice([32]),#[16, 8]),
-            "loss_type": tune.choice(['MSELoss', 'MAELoss']),  # ['MARELoss']
-            "Y_features": tune.choice([['LC']]),
-            # [['LC'], ['r0', 'r1', 'LC'], ['r0', 'r1'], ['r0', 'r1', 'dr'], ['r0', 'r1', 'dr', 'LC']]
-            "use_power": tune.grid_search([True, False]),
-            "use_bg": tune.grid_search([False]),  # True - bg is relevant for 'lidar' case # TODO if lidar - bg T\F, if signal - bg F
-            "source": tune.grid_search(['signal', 'lidar']),
-            'data_filter': tune.grid_search([('wavelength', [355]), None])
-        }
-
+    if USE_RAY:
         reporter = CLIReporter(
             metric_columns=["loss", "MARELoss", "training_iteration"])
 
         analysis = tune.run(
-            tune.with_parameters(main, consts=consts),
-            config=hyper_params,
-            local_dir=os.path.join(base_path, 'results'),  # where to save the results
+            tune.with_parameters(main, consts=CONSTS),
+            config=RAY_HYPER_PARAMS,
+            local_dir=RESULTS_PATH,  # where to save the results
             fail_fast=True,  # if one run fails - stop all runs
             metric="MARELoss",
             mode="min",
             progress_reporter=reporter,
             log_to_file=True,
-            resources_per_trial={"cpu": 7, "gpu": num_available_gpu})
+            resources_per_trial={"cpu": 7, "gpu": NUM_AVAILABLE_GPU})
 
         logger.info(f"best_trial {analysis.best_trial}")
         logger.info(f"best_config {analysis.best_config}")
@@ -133,18 +95,4 @@ if __name__ == '__main__':
         logger.info(f"best_checkpoint {analysis.best_checkpoint}")
         logger.info(f"best_result {analysis.best_result}")
     else:
-        hyper_params = {
-            "lr": 1 * 1e-3,
-            "bsize": 8,
-            "loss_type": 'MSELoss',
-            "Y_features": ['LC'],
-            "use_power": True,
-            "use_bg": False,
-            "source": 'signal',
-            "hidden_sizes": [16, 32, 8],
-            "source": 'signal',
-            'data_filter': None,
-
-        }
-
-        main(hyper_params, consts)
+        main(NON_RAY_HYPER_PARAMS, CONSTS)
