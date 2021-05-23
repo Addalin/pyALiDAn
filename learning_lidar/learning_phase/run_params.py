baed8c4d5127354e06c4f38d05f164113b16d7c4
Addalin <adi.vainiger@gmail.com>
2021-05-23 19:08:13 +0300

    commit before pull

--- learning_lidar/learning_phase/run_params.py
+++ learning_lidar/learning_phase/run_params.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import torch
 from ray import tune
 import json
+
 NUM_AVAILABLE_GPU = torch.cuda.device_count()
 
 
@@ -23,7 +24,6 @@ def get_paths(station_name, start_date, end_date):
 train_csv_path, test_csv_path, stats_csv_path, RESULTS_PATH = get_paths(station_name='haifa',
                                                                         start_date=datetime(2017, 9, 1),
                                                                         end_date=datetime(2017, 10, 31))
-
 
 experiment_dir = 'main_2021-05-19_23-17-25'
 trial_dir = r'main_39b80_00000_0_bsize=32,dfilter=None,dnorm=True,fc_size=[16],hsizes=[3, 3, 3, 3],lr=0.001,ltype=MAELoss,source=lidar,use_bg=Tr_2021-05-19_23-17-25'
@@ -56,24 +56,23 @@ CONSTS = {
 
 # Note, replace tune.choice with grid_search if want all possible combinations
 RAY_HYPER_PARAMS = {
-    "hsizes": tune.grid_search(['[3, 3, 3, 3]', '[4, 4, 4, 4]', '[5, 5, 5, 5]']),  # '[1,1,1,1]','[2,2,2,2]',
+    "hsizes": tune.grid_search(['[3, 3, 3, 3]', '[4, 4, 4, 4]', '[5, 5, 5, 5]']),  # '[1,1,1,1]', '[2,2,2,2]',
     "fc_size": tune.grid_search(['[16]', '[32]']),  # '[4]',
-    "lr": tune.choice([1 * 1e-3]),  # [1e-3, 0.5 * 1e-3, 1e-4]),
-    "bsize": tune.choice([32]),  # 48 , 64]),  # [16, 8]),
+    "lr": tune.grid_search([1 * 1e-3, 2 * 1e-3, 4 * 1e-3]),  # 0.5 * 1e-3, 1e-4,1 * 1e-3  - later for hsizes of :1,2
+    "bsize": tune.grid_search([32]),  # 48 , 64]),  # [16, 8]),
     "ltype": tune.choice(['MAELoss']),  # , 'MSELoss']),  # ['MARELoss']
     # [['LC'], ['r0', 'r1', 'LC'], ['r0', 'r1'], ['r0', 'r1', 'dr'], ['r0', 'r1', 'dr', 'LC']]
-    "use_power": tune.grid_search([  # False,
-        "([0.5, 0.5, 0.5], [0.5])",
-        "([0.5,-0.2, 0.5], [0.5])",
-        "([0.5,0.25, 0.5], [0.5])",
-        "([0.5,-0.25, 0.5],[0.5])"]),
+    "use_power": tune.grid_search(["([0.5, -0.25], [0.5])"]),
+    # "([0.5,-0.2, 0.5], [0.5])",
+    # "([0.5,0.25, 0.5], [0.5])",
+    # "([0.5,-0.25, 0.5],[0.5])"]),
     # "([0.5, 0.25], [0.5])", "([0.5, 0.25], [1])",
     # "([0.5, -0.25], [0.5])", "([0.5, -0.25], [1])"]),
-    "use_bg": tune.grid_search(['range_corr']),  #  False, True,'range_corr'
+    "use_bg": tune.grid_search([False]),  # ,'range_corr'
     # True - bg is relevant for 'lidar' case # TODO if lidar - bg T\F, if signal - bg F
-    "source": tune.grid_search(['lidar']),  # , 'signal', 'signal_p'
+    "source": tune.grid_search(['signal', 'signal_p' ]),  # , 'lidar'
     'dfilter': tune.grid_search([None]),  # , ('wavelength', [355])]), # data_filter
-    'dnorm': tune.grid_search([False]),  # data_norm, , False, True
+    'dnorm': tune.grid_search([False]),  # data_norm True - only for the best results achieved.
 }
 
 RESTORE_TRIAL_PARAMS = {
@@ -95,11 +94,11 @@ NON_RAY_HYPER_PARAMS = {
     "lr": 1 * 1e-3,
     "bsize": 32,
     "ltype": 'MAELoss',  # loss_type
-    "use_power": ([0.5, 0.25, 0.5], [0.5]),
+    "use_power": '[0.5, -0.25], [0.5]',
     "use_bg": True,
-    "source": 'signal',
-    "hsizes": [3, 3, 3, 3],  # hidden_sizes
-    "fc_size": [16],
+    "source": 'signal_p',
+    "hsizes": '[3, 3, 3, 3]',  # hidden_sizes
+    "fc_size": '[16]',
     'dfilter': None,  # data_filter
     'dnorm': True,  # data_norm
 }
