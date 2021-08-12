5a28267c411076927002cccdc0e87cb919a8225c
Addalin <adi.vainiger@gmail.com>
2021-08-12 13:23:17 +0300

    dump all run parameters to all_params.yaml

--- learning_lidar/learning_phase/main_lightning.py
+++ learning_lidar/learning_phase/main_lightning.py
@@ -1,5 +1,6 @@
 import logging
 import os.path
+import yaml
 from datetime import datetime
 
 import ray
@@ -21,7 +22,9 @@ seed_everything(8318)  # Note, for full deterministic result add deterministic=T
 # https://github.com/ray-project/ray/blob/35ec91c4e04c67adc7123aa8461cf50923a316b4/python/ray/tune/examples/mnist_pytorch_lightning.py
 
 def main(config, checkpoint_dir=None, consts=None):
-
+    with open('all_params.yaml', 'a') as f:
+        yaml.dump(consts, f)
+        yaml.dump(config, f)
     config, X_features, powers = update_params(config, consts)
 
     # Define Model
