1138a1f28583e35269242788fd908c55b363fb2d
Addalin <adi.vainiger@gmail.com>
2022-02-06 13:04:27 +0200

    update description in setup.py and add TODO for future update

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-# TODO Fill in fields, or recreate from scratch with Pycharm: Tools --> create setup.py
 setup(
     name='learning_lidar',
     version='0.1',
@@ -7,9 +6,19 @@ setup(
               'learning_lidar.generation', 'learning_lidar.preprocessing', 'learning_lidar.learning_phase',
               'learning_lidar.learning_phase.models', 'learning_lidar.learning_phase.utils_',
               'learning_lidar.learning_phase.data_modules'],
-    url='',
-    license='',
-    author='Adi',
-    author_email='adi',
-    description='Learning Lidar'
+    url='https://github.com/Addalin/learning_lidar',
+    license='MIT',
+    author='Adi Vainiger',
+    author_email='adi.vainiger@gmail.com , addalin@campus.technion.ac.il',
+    description='Learning Lidar is an atmospheric lidar learning repo. The repo includes:'
+                '1. ALiDAn: Atmospheric Lidar Data Augmentation. Statistical data generation'
+                '2. Preprocessing of lidar data'
+                '2. Learning pipeline for lidar analysis'
+                'This project uses data from several databases:'
+                '1. Lidar database produced by the PollyNet Processing chain  by TROPOS: '
+                '    https://github.com/PollyNET/Pollynet_Processing_Chain '
+                ' 2. AERONET by NASA: https://aeronet.gsfc.nasa.gov/new_web/data.html'
+                ' 3. GDAS by NOAA: https://www.ncei.noaa.gov/products/weather-climate-models/global-data-assimilation'
+                ' * Additional Databases are possible as well. More details can be found in the ALiDAn manuscript'
 )
+# TODO: update information on ALiDAn
